e5b3323b66df5f2251fc684b96146fdd92e31911
Ji Seong Ham <hgs3896@naver.com>
2024-01-09 23:55:17 +0900

    fix: JS not found error

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1704810845';
+const cacheName = 'chirpy-1704811923';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
