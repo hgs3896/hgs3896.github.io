a1ea709ee7468739b29f851cc56650fee9e98367
Ji Seong Ham <hgs3896@naver.com>
2024-01-10 00:09:26 +0900

    Add sitemap.xml

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1704811923';
+const cacheName = 'chirpy-1704812800';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
