fb75a68e07e46b6f6551b6965a4712881d3ef733
Ji Seong Ham <hgs3896@naver.com>
2024-01-10 00:15:11 +0900

    Update

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1704812800';
+const cacheName = 'chirpy-1704813267';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
